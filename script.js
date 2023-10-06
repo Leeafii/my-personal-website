f006c1cee98b04d48b16bda7831af0290d7effe5
Titus <tituslee09@gmail.com>
2023-10-07 08:29:26 +1100

    added functino wordmap and defined words to numbers

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function generateRandomNumber(min, max) {
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     return randomNumber;
 }
+
+function mapNumberToWord(number) {
+    const numberWordMap = {
+        1: "Low",
+        2: "Below Average",
+        3: "Average",
+        4: "Above Average",
+        5: "High",
+        6: "Very High",
+        7: "Excellent",
+        8: "Exceptional",
+        9: "Superhuman",
+        10: "Godlike"
+    };
+}
